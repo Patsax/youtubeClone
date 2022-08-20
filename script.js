f515034f1ef63b98ca98e5cc6e3316045e5ea89e
Patrick <patasax22@yahoo.com>
2022-08-20 13:15:26 -0400

    play/pause complete

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const playPauseBtn = document.querySelector(".play-pause-btn");
-const videoContainer = document.querySelector('.video-container');
-const video = document.querySelector('video');
+const videoContainer = document.querySelector(".video-container");
+const video = document.querySelector("video");
 
-playPauseBtn.addEventListener('click', togglePlay)
+document.addEventListener("keydown", (e) => {
+	switch (e.key.toLowerCase()) {
+		case " ":
+		case "k":
+			togglePlay();
+			break;
+	}
+});
+
+// Play/Pause options
+playPauseBtn.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
 
 function togglePlay() {
-    video.paused ? video.play() : video.pause()
+	video.paused ? video.play() : video.pause();
 }
 
-video.addEventListener('play', () => {
-    videoContainer.classList.remove('paused')
-})
-video.addEventListener('pause', () => {
-    videoContainer.classList.add('paused')
-})
+video.addEventListener("play", () => {
+	videoContainer.classList.remove("paused");
+});
+video.addEventListener("pause", () => {
+	videoContainer.classList.add("paused");
+});
