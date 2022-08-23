124b05b9ff3fdfb1fbb83f8afa8c1f9ebb844040
Patrick <patasax22@yahoo.com>
2022-08-23 14:48:25 -0400

    volume control complete

--- script.js
+++ script.js
@@ -2,17 +2,19 @@ const playPauseBtn = document.querySelector(".play-pause-btn");
 const miniPlayerBtn = document.querySelector(".mini-player-btn");
 const theaterBtn = document.querySelector(".theater-btn");
 const fullScreenBtn = document.querySelector(".full-screen-btn");
+const muteBtn = document.querySelector(".mute-btn");
+const volumeSlider = document.querySelector(".volume-slider");
 const videoContainer = document.querySelector(".video-container");
 const video = document.querySelector("video");
 
 document.addEventListener("keydown", (e) => {
-	const tagName = document.activeElement.tagName.toLowerCase()
+	const tagName = document.activeElement.tagName.toLowerCase();
 
-	if (tagName === 'input') return
+	if (tagName === "input") return;
 
 	switch (e.key.toLowerCase()) {
 		case " ":
-			if (tagName === 'button') return
+			if (tagName === "button") return;
 		case "k":
 			togglePlay();
 			break;
@@ -25,7 +27,36 @@ document.addEventListener("keydown", (e) => {
 		case "i":
 			toggleMiniPlayerMode();
 			break;
+		case "m":
+			toggleMute();
+			break;
 	}
+});
+
+// Volume control
+muteBtn.addEventListener("click", toggleMute);
+volumeSlider.addEventListener("input", (e) => {
+	video.volume = e.target.value;
+	video.muted = e.target.value === 0;
+});
+
+function toggleMute() {
+	video.muted = !video.muted;
+}
+
+video.addEventListener("volumechange", () => {
+	volumeSlider.value = video.volume;
+	let volumeLevel;
+	if (video.muted || video.volume === 0) {
+		volumeSlider.value = 0;
+		volumeLevel = "muted";
+	} else if (video.volume >= 0.5) {
+		volumeLevel = "high";
+	} else {
+		volumeLevel = "low";
+	}
+
+	videoContainer.dataset.volumeLevel = volumeLevel;
 });
 
 // View Options
